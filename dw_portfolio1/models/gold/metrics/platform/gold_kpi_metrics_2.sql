d5383dbfe55f85fa643a2bd89bebb6b1df129162
feelipensousa <felipenogueirafebob11@gmail.com>
2025-09-07 21:34:42 -0300

    adicionando novo kpi na gold de metricas

--- dw_portfolio1/models/gold/metrics/platform/gold_kpi_metrics_2.sql
+++ dw_portfolio1/models/gold/metrics/platform/gold_kpi_metrics_2.sql
@@ -1,1 +1,10 @@
--- 2) maiores cpc, ctr, cpa por plataforma
+-- 2) custo total e CPA por plataforma
+
+WITH source AS (
+    platform,
+    SUM(cost) as total_cost,
+    SUM(cost) / NULLIF(SUM(conversions), 0) AS cpa
+    FROM {{ ref('silver_products_metrics') }}
+    GROUP BY platform
+),
+SELECT * FROM source  
